b24e79b698ac33bccbba8a19fdf2727d84bda516
anig1scur <anigiscur@gmail.com>
2024-10-04 23:51:45 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # ocr-word-cropper
+
+This project is an image processing tool based on EasyOCR, designed to recognize specific text in images and crop the corresponding areas.
+
+<div align="center" style="display: flex;gap:15px">
+    <img src="./images/milk.jpg" width="45%"  />
+    <img src="./output/milk/res.png" width="50%"  />
+</div>
+
+### Dependencies
+
+- EasyOCR
+- Pillow
+
+### Usage
+
+1. Install dependencies: `pip install -r requirements.txt`
+2. Config the target words in `config.py`
+3. Run!
+
+### Todo
+
+1. API for upload image to github
+2. Only crop the target words
+3. GUI for easy usage
+
+### Ref
+
+Thanks to `EasyOCR`!
--- api/process.py
+++ api/process.py
@@ -1,39 +1,10 @@
 from http.server import BaseHTTPRequestHandler
-import easyocr
-from PIL import Image
-import io
 import base64
 import json
 
+from ..main import ocr_and_crop_words_for_api
 
-def ocr_and_crop_words(image_bytes, target_words):
-    reader = easyocr.Reader(["ch_sim", "en"])
-    image = Image.open(io.BytesIO(image_bytes))
-
-    result = reader.readtext(image)
-
-    cropped_images = []
-
-    for idx, (bbox, text, prob) in enumerate(result):
-        if any(target_word in text for target_word in target_words):
-            top_left, top_right, bottom_right, bottom_left = bbox
-            left, top = map(int, top_left)
-            right, bottom = map(int, bottom_right)
-
-            left = max(0, left - 2)
-            top = max(0, top - 2)
-            right = min(image.width, right + 2)
-            bottom = min(image.height, bottom + 2)
-
-            cropped_image = image.crop((left, top, right, bottom))
-
-            buffered = io.BytesIO()
-            cropped_image.save(buffered, format="PNG")
-            img_str = base64.b64encode(buffered.getvalue()).decode()
-
-            cropped_images.append({"text": text, "image": img_str})
-
-    return cropped_images
+# vercel serverless 内存限制所以跑不了 .....
 
 
 class handler(BaseHTTPRequestHandler):
@@ -45,7 +16,7 @@ class handler(BaseHTTPRequestHandler):
         image_data = base64.b64decode(data["image"])
         target_words = data["target_words"]
 
-        result = ocr_and_crop_words(image_data, target_words)
+        result = ocr_and_crop_words_for_api(image_data, target_words)
 
         self.send_response(200)
         self.send_header("Content-type", "application/json")
